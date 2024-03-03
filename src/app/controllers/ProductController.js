df055f0f8fad1d836f569e14d3c3f88343933550
Romildo <romildo.silvafilho1@gmail.com>
2024-03-03 15:50:06 -0300

    feat: implement createMany products route

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,8 +2,7 @@ const ProductService = require('../services/ProductServices');
 
 class ProductController {
   async createOne(req, res) {
-    const { name, details, price } = req.body;
-    let { brand, model, color } = req.body;
+    let { name, details, price, brand, model, color } = req.body;
 
     if (details) {
       brand = details.brand;
@@ -21,11 +20,52 @@ class ProductController {
       model,
       color,
       price,
-    }
+    };
 
     const {type, message} = await ProductService.create(payload);
 
     return res.status(type).json(message);
+  };
+
+  async createMany(req, res) {
+    const { products } = req.body;
+
+    if (!products || products.length === 0) {
+      return res.status(404).json({message: "At least one product must be specified"});
+    };
+
+    const newProducts = await Promise.all(products.map(async(product, index) => {
+      const { name, brand, model, data } = product;
+
+      if (!name || !brand || !model || !data) {
+        return { type: 500, messsage: "All fields are required" }
+      }
+      const response = await Promise.all(product.data.map(async(details, subIndex) => {
+        const { price, color } = details;
+
+        if (!price || isNaN(price) || !color) {
+          return { type: 500, messsage: `Data fields malformed on index [${index}, ${subIndex}]` }
+        };
+
+        const payload = {
+          name: product.name,
+          brand: product.brand,
+          model: product.model,
+          price: details.price,
+          color: details.color,
+        };
+
+        const newProduct = await ProductService.create(payload);
+        return newProduct;
+      }));
+      return response;
+    }));
+
+    if (newProducts?.length) {
+      return res.status(201).json(newProducts.flat());
+    };
+
+    return res.status(500).json({message: "Error creating new products"});
   };
 };
 
--- src/routers/ProductRoutes.routes.js
+++ src/routers/ProductRoutes.routes.js
@@ -3,11 +3,8 @@ const router = express.Router();
 const ProductController = require('../app/controllers/ProductController');
 
 router.post('/', ProductController.createOne);
-// router.post('/admin', UserController.createAdmin);
-// router.post('/login', UserController.login);
-// router.get('/', UserController.getAll);
-// router.get('/:id', UserController.getById);
-// router.patch('/:id', UserController.update);
+router.post('/many', ProductController.createMany);
+// router.patch('/:id', ProductController.update);
 // router.delete('/:id', UserController.delete);
 
 module.exports = router;
