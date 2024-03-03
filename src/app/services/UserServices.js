c90ccada7f492ac95ddd03ad758878c5eb95ea3b
Romildo <romildo.silvafilho1@gmail.com>
2024-03-02 23:25:56 -0300

    test: console log error create admin

--- src/app/services/UserServices.js
+++ src/app/services/UserServices.js
@@ -16,6 +16,7 @@ class UserService {
       
       return {type: 201, message: response};
     } catch (error) {
+      console.log('erro da query', error);
       return { type: 500, message: error.message };
     };
   };
@@ -34,6 +35,7 @@ class UserService {
       
       return {type: 201, message: response};
     } catch (error) {
+      console.log('erro da query', error);
       return { type: 500, message: error };
     };
   };
